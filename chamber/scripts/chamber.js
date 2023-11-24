f226f8a1edf4f138a79e364933251a2d181020c8
ggosdinski <gianfranco.gosdinski@icloud.com>
2023-11-23 21:15:02 -0500

    Correcciones a la pagina Discover

--- chamber/scripts/chamber.js
+++ chamber/scripts/chamber.js
@@ -9,34 +9,38 @@ document.addEventListener('DOMContentLoaded', function () {
 
 document.getElementById('lastModified').innerText = document.lastModified;
 
+
+
+
 // Function to get the time difference between two dates in days
 function getDaysDifference(currentDate, lastVisitDate) {
     const oneDay = 24 * 60 * 60 * 1000; // Number of milliseconds in a day
-    return Math.round(Math.abs((currentDate - lastVisitDate) / oneDay));
+    return Math.floor(Math.abs((currentDate - lastVisitDate) / oneDay));
 }
 
 // Get the current date and time
 const currentDate = new Date();
+const visitMessage = document.querySelector('.visit-message');
 
-// Retrieve last visit date from localStorage
 const lastVisit = localStorage.getItem('lastVisit');
 
 if (lastVisit === null) {
     // First visit - display welcome message
-    document.querySelector('.demographics-info').innerHTML = "<h2>Welcome! Let us know if you have any questions.</h2>";
+    visitMessage.textContent = "Welcome! Let us know if you have any questions.";
 } else {
     const lastVisitDate = new Date(lastVisit);
     const daysDifference = getDaysDifference(currentDate, lastVisitDate);
 
     if (daysDifference < 1) {
         // Less than a day - display message
-        document.querySelector('.demographics-info').innerHTML = "<h2>Back so soon! Awesome!</h2>";
+        visitMessage.textContent = "Back so soon! Awesome!";
     } else {
         // More than a day - display days difference
         const daysText = daysDifference === 1 ? 'day' : 'days';
-        document.querySelector('.demographics-info').innerHTML = `<h2>You last visited ${daysDifference} ${daysText} ago.</h2>`;
+        visitMessage.textContent = `You last visited ${daysDifference} ${daysText} ago.`;
     }
 }
 
 // Store current visit date in localStorage
 localStorage.setItem('lastVisit', currentDate.toISOString());
+
